aa31365068f02ede786050ff35da8e3a3823f8b7
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-21 18:35:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+    erb :index
+  end
+
+  post '/' do
+    @analyzed_text = TextAnalyzer.new(params[:user_text])
+    
+    erb :results
+  end
 end
